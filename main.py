0f59e83ca5fbbc8328231dd8435ce26153b9c301
Sherloth <dl.kmiecik@gmail.com>
2025-04-05 21:42:03 +0100

    changed loop to len()

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ def get_book_text():
         content = f.read()
     return content
 
-# create a list of strings - words and count them
+# create a list of strings
 def word_count():
     text = get_book_text()
     split_list = text.split()
-    count = 0
-
-    # count strings with loop
-    for word in split_list:
-        count += 1
-    return count
+    return split_list
 
 # define main function - print text stating the amount of words
 def main():
-    num_words = word_count()
+    num_words = len(word_count())
     print(f"{num_words} words found in the document")
 
 # call function
