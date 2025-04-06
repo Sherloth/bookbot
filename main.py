ac053ca26de369dc7921278ab81174fdfd4205a7
Sherloth <dl.kmiecik@gmail.com>
2025-04-06 20:02:30 +0100

    output format

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # imports
 from stats import word_count
+from stats import letter_count
+
+book_path = "books/frankenstein.txt"
 
 # define main function - print text stating the amount of words
 def main():
     num_words = word_count()
-    print(f"{num_words} words found in the document")
+    dictionary = letter_count()
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    print(f"{dictionary}")
+    
 
 # call function
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,14 @@ def word_count():
     split_list = text.split()
     return len(split_list)
 
+# create dictionary of lowercase letter counts
+def letter_count():
+    new_list = list(get_book_text())
+    dictionary = {}
+    for letter in list(new_list):
+        letter = letter.lower()
+        if letter not in dictionary:
+            dictionary[letter] = 1
+        elif letter in dictionary:
+            dictionary[letter] += 1
+    return dictionary
