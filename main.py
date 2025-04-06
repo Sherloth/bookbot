d9478d9ec29f3ceeadcfbfa7284c8ddff78c906c
Sherloth <dl.kmiecik@gmail.com>
2025-04-06 22:13:17 +0100

    added sys.argv

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # imports
 from stats import word_count, characters_dictionary
+import sys
 
-book_path = "books/frankenstein.txt"
+# sys.agrv check
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 # define main function - print text stating the amount of words
 def main():
-    num_words = word_count()
-    dictionary = characters_dictionary()
+    num_words = word_count(book_path)
+    dictionary = characters_dictionary(book_path)
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
@@ -14,7 +20,7 @@ def main():
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     for item in dictionary:
-        print(f"{item["char"]} : {item["count"]}")
+        print(f"{item["char"]}: {item["count"]}")
     
 
 # call function
--- stats.py
+++ stats.py
@@ -1,19 +1,18 @@
 # get text from file an string
-def get_book_text():
-    book_path = "books/frankenstein.txt"
+def get_book_text(book_path):
     with open(book_path) as f:
         content = f.read()
     return content
 
 # create a list of strings
-def word_count():
-    text = get_book_text()
+def word_count(book_path):
+    text = get_book_text(book_path)
     split_list = text.split()
     return len(split_list)
 
 # create dictionary of lowercase letter counts
-def letter_count():
-    new_list = list(get_book_text())
+def letter_count(book_path):
+    new_list = list(get_book_text(book_path))
     dictionary = {}
     for letter in list(new_list):
         letter = letter.lower()
@@ -25,10 +24,10 @@ def letter_count():
     return dictionary
 
 # dictionary of characters
-def characters_dictionary():
-    d_in = letter_count()
+def characters_dictionary(book_path):
+    d_in = letter_count(book_path)
     d_out = []
     for char, count in d_in.items():
-        d_out.append({"char" : char, "count" : count})
+        d_out.append({"char": char, "count" : count})
     d_out.sort(key=lambda x: x["count"], reverse=True)
     return d_out
